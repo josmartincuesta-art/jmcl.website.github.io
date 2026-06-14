981abe37d9e4b2e2cb246dd57a258fda6f26e3e7
jmcl_eDigital <josmartin.cuesta@gmail.com>
2026-06-14 12:53:15 +0200

    Integrate Google Custom Search Engine script
    Added Google Custom Search Engine script for integration.

--- docs/js/buscalotodo.js
+++ docs/js/buscalotodo.js
@@ -1,2 +1,12 @@
 <script async src="https://cse.google.com/cse.js?cx=63eba9f9a56774d81">
 </script>
+
+(function() {
+  var cx = '63eba9f9a56774d81';
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+})();
